094b837a4c6ed07cf7997e3731a6e812ef001bc4
Joseph Ciesielski <jtcies@gmail.com>
2018-08-17 11:36:13 -0400

    starting to add real games from each date

--- src/app_compare.R
+++ src/app_compare.R
@@ -46,7 +46,9 @@ ui <- ui <- fluidPage(
       textOutput("home_win_prob"),
       h3("away team"),
       textOutput("vis_elo"),
-      textOutput("vis_win_prob")
+      textOutput("vis_win_prob"),
+      h2("real games on this day"),
+      tableOutput("games_date")
     )
   )
 )
@@ -56,21 +58,53 @@ server <- function(input, output, session) {
   
   home_dat <- reactive({
     elo %>% 
-    filter(
-      pretty_name == input$home_team,
-      date == input$date
-    )
+      filter(
+        pretty_name == input$home_team,
+        date == input$date
+      )
   })
   
-  vis_dat <- reactive({ elo %>% 
-    filter(
-      pretty_name == input$vis_team,
-      date == input$date
-    )
+  vis_dat <- reactive({ 
+    elo %>% 
+      filter(
+        pretty_name == input$vis_team,
+        date == input$date
+      )
   })
+
   
   prob <- reactive({
     elo.prob(home_dat()$elo + 100, vis_dat()$elo - 100)
+  })
+  
+  
+  date_games <- reactive({
+    
+    dat <- scores %>% 
+      filter(date == test_date) %>% 
+      left_join(
+        elo %>% 
+          rename(vis_elo = elo) %>% 
+          filter(date %in% c(test_date, test_date - 1)),
+        by = c("vis_team" = "team")
+      ) %>% 
+      left_join(
+        elo %>% 
+          rename(home_elo = elo) %>% 
+          filter(date %in% c(test_date, test_date -1)),
+        by = c("home_team" = "team")
+      ) %>% 
+      arrange(date) %>% 
+      mutate(
+        prev_home_elo = lag(home_elo),
+        prev_vis_elo = lag(vis_elo)
+      ) %>% 
+      filter(date == test_date) %>% 
+      select(visitor, visitor_pts, home, home_pts,
+             prev_vis_elo, prev_home_elo,
+             vis_elo_after = vis_elo, home_elo_after = home_elo)
+    
+    dat
   })
   
   output$home_elo <- renderText({
@@ -83,6 +117,9 @@ server <- function(input, output, session) {
   
   output$home_win_prob <- renderText({make_pct(prob())})
   output$vis_win_prob <- renderText({make_pct(1 - prob())})
+  
+  output$games_date <- renderTable({date_games()})
 }
+
 
 shinyApp(ui, server)
